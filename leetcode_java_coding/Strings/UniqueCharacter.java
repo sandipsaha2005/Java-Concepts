bf0346526f27594324821e5a3739501e8cff233d
Sandip Saha <110690069+sandipsaha2005@users.noreply.github.com>
2024-03-10 16:40:14 +0530

    Update UniqueCharacter.java
    adding the code of gettting first unique character

--- leetcode_java_coding/Strings/UniqueCharacter.java
+++ leetcode_java_coding/Strings/UniqueCharacter.java
@@ -1,1 +1,18 @@
+public static int getFirstUniqueCharacter(String str){
+        Queue<Character> q=new LinkedList<>();
+        int arr[]=new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            q.add(str.charAt(i));
+            arr[str.charAt(i)-97]+=1;
+        }
+        int i=0;
+        for(Character item : q){
+            char ch=item;
 
+            if(arr[ch-97]==1){
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
